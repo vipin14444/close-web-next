56129cd5a6db2d3416358e526c6d7ec72b2329b5
vipin14444 <vipin.sharma3039@gmail.com>
2021-05-10 10:59:20 +0530

    initial commit

--- config/GlobalConfig.js
+++ config/GlobalConfig.js
@@ -1,4 +1,5 @@
 module.exports = {
     baseUrl: 'http://13.234.111.253:9000',
-    clientApiBaseUrl: 'http://localhost:3000/api'
+    // clientApiBaseUrl: 'http://localhost:3000/api',
+    clientApiBaseUrl: 'https://close-web-next.vercel.app/api',
 }
